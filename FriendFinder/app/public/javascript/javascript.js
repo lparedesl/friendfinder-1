f0c8e838f88350bec302a30fd2a979c4b60d29df
Luis Paredes <lparedesl@gmail.com>
2017-05-29 08:44:13 -0400

    fixed

--- FriendFinder/app/public/javascript/javascript.js
+++ FriendFinder/app/public/javascript/javascript.js
@@ -9,6 +9,72 @@ $(document).ready(function() {
     // audio.volume = 0.2;
     $("#onLoad").prop("volume", 0.2);
 
+    // function fade() {
+    //     fadeGenderChoice();
+    // }
+
+    function fadeGenderChoice() {
+        //begin fade in and out of first question
+        // Show question div
+        $("#first").fadeIn(2000).removeClass('hiddenfirst');
+        // Show cards div
+        $("#second").fadeIn(5000).removeClass('hiddenfirst');
+
+        // Hide question div
+        $("#first").fadeOut(1000, function() {
+            // Show first question
+            $(this).html("<h1>Lord or Lady?</h1>").fadeIn(1000);
+            // fadeGenderCards();
+
+            // Show first set of cards
+            $("#second").fadeOut(1000, function() {
+                $(this).html("<img id = 'male' class='gender' data-gender='male' src = 'images/male.jpg'>&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; <img id = 'female' class='gender' data-gender='female' src = 'images/female.png'>").fadeIn(1000);
+                // Enable hover effect
+                hoverEffect();
+            });
+        });
+    }
+
+    // Select gender
+    $(document).on("click", ".gender", function() {
+        selectedGender = $(this).data("gender");
+        if(selectedGender === "female") {
+            console.log("You are a pretty woman");
+            $("#male").fadeOut(2000);
+        } else if (selectedGender === "male") {
+            console.log("You are a handsome man");
+            $("#female").fadeOut(2000);
+        }
+
+        // Hide question div
+        $("#first").fadeOut(8000,function() {
+            // Show second question
+            $(this).html("<h1>Lord or Lady?</h1>").fadeIn(3000);
+            // Show second set of cards
+            $("#second").fadeOut(5000, function() {
+                $(this).html("<img id = 'male' class='genderPref' data-gender='male' src = 'images/male_2.png'>&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; <img id = 'female' class='genderPref' data-gender='female' src = 'images/female_2.png'>").fadeIn(5000);
+                hoverEffect();
+            });
+        });
+
+        // var userGender = {
+        // 	gender: selectedGender
+        // };
+        // var currentURL = window.location.origin;
+        // $.post(currentURL + "/gender", userGender, function(data) {
+        // });
+    });
+
+    // Select gender preference
+    $(document).on("click", ".genderPref", function() {
+        selectedGender = $(this).data("gender");
+        if(selectedGender === "female") {
+            console.log("You chose female");
+        } else if (selectedGender === "male") {
+            console.log("You chose male");
+        }
+    });
+
     function moveForward() {
         $("#first").fadeOut(8000, function() {
             $(this).html().fadeIn(3000);
@@ -27,112 +93,27 @@ $(document).ready(function() {
         moveForward();
     }
 
-    //creating the hover function
+    // creating the hover function
     function hoverEffect() {
-        console.log("this works");
         //play the card loading sound
         $("#effect").get(0).play();
         //play the hover sounds for guy and girl
-        $("#male").mouseenter(function() {
+        $("#male, #female").mouseenter(function() {
             console.log("this works");
             $("#hover").get(0).play();
         });
-        $("#male").mouseleave(function() {
+
+        $("#male, #female").mouseleave(function() {
             $("#hover").get(0).pause();
             hover.currentTime = 0;
         });
-        $("#female").mouseenter(function() {
-            $("#hover").get(0).play();
-        });
-        $("#female").mouseleave(function() {
-            $("#hover").get(0).pause();
-            hover.currentTime = 0;
-        });
-
-        $(document).on("click", ".gender", function() {
-            selectGender(this);
-
-        });
-    }
-
-    function fadePrefCards() {
-        // var male = document.getElementById("male");
-        // var female = document.getElementById("female");
-
-        $("#second").fadeOut(5000, function() {
-            $(this).html("<img id = 'male' class='gender' data-gender='male' src = 'images/male_2.png'>&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; <img id = 'female' class='gender' data-gender='female' src = 'images/female_2.png'>").fadeIn(5000);
-            hoverEffect();
-            return;
-        });
-        return;
-    }
-
-    function fadeUserPref() {
-        //begin fade in and out of first question
-        $("#first").fadeOut(8000,function() {
-            $(this).html("<h1>Lord or Lady?</h1>").fadeIn(3000);
-            fadePrefCards();
-            return;
-        });
-        return;
-    }
-
-    function selectGender(gender) {
-        selectedGender = $(gender).data("gender");
-        console.log(gender);
-        console.log('SELECTEDGENDER: ' +  selectedGender);
-        if(selectedGender === "female") {
-            console.log("You are a pretty woman");
-            $("#male").fadeOut(2000);
-            fadeUserPref();
-            return;
-        } else if (selectedGender === "male") {
-            console.log("You are a handsome man");
-            $("#female").fadeOut(2000);
-            fadeUserPref();
-            return;
-        }
-
-        // var userGender = {
-        // 	gender: selectedGender
-        // };
-        // var currentURL = window.location.origin;
-        // $.post(currentURL + "/gender", userGender, function(data) {
-        // });
-    }
-
-    function fadeGenderCards() {
-        // var male = document.getElementById("male");
-        // var female = document.getElementById("female");
-
-        $("#second").fadeOut(1000,function() {
-            $(this).html("<img id = 'male' class='gender' data-gender='male' src = 'images/male.jpg'>&nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; <img id = 'female' class='gender' data-gender='female' src = 'images/female.png'>").fadeIn(1000);
-            hoverEffect();
-        });
-        return;
-    }
-
-    function fadeGenderChoice() {
-        //begin fade in and out of first question
-        $("#first").fadeIn(2000).removeClass('hiddenfirst');
-        $("#second").fadeIn(5000).removeClass('hiddenfirst');
-
-        $("#first").fadeOut(1000,function() {
-            $(this).html("<h1>Lord or Lady?</h1>").fadeIn(1000);
-            fadeGenderCards();
-        });
-        return;
-    }
-
-    function fade() {
-        fadeGenderChoice();
     }
 
     function onStart() {
         //play music
         $("#onLoad").get(0).play();
 
-        fade();
+        fadeGenderChoice();
     }
 
     onStart();
